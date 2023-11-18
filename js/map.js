3644bd16acce38a1c48ecb1421c0d2c65fbc9e92
emartinez5 <emartinez5@hawk.iit.edu>
2023-11-17 23:45:34 -0600

    updates
    added js and fixed spacing

--- js/map.js
+++ js/map.js
@@ -1,12 +1,20 @@
 let map;
 
 async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-
+  const funkoHQ = { 47.98040880624634, -122.20758740998346 }; 	
   map = new Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
 }
+
+  const marker = new google.maps.Marker({
+	  position: funkoHQ,
+	  map: map, 
+	  title: 'Funko HQ'
+	  icon: 'images/funkoICON.png'
+  });
+
+
 
 initMap();
